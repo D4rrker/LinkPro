00571be10383fc20f9f6e9a5618a3e0b39cd3a29
D4rrker <holadarkerktal@gmail.com>
2025-02-08 20:03:30 +0100

    Solución en la ruta dinámica 'params'

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -3,7 +3,6 @@ import { getOriginalUrl } from "@/services/fetchServices";
 type tParams = Promise<{ shortId: string }>;
 
 export default async function ShortUrlPage({ params }: { params: tParams }) {
-  // Este await aparece como que no es necesario, pero sí tiene efecto.
   const { shortId }: { shortId: string } = await params;
   await getOriginalUrl(shortId);
 
