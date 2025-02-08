1cf8bde85bf2cabcf66fb672b6ba0fd89b74c0d4
D4rrker <holadarkerktal@gmail.com>
2025-02-08 19:29:06 +0100

    Se quitó el 'await' al obtener 'params' de '[shortId]'

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -6,6 +6,6 @@ export default async function ShortUrlPage({
   params: { shortId: string };
 }) {
   // Este await aparece como que no es necesario, pero sí tiene efecto.
-  const { shortId } = await params;
+  const { shortId } = params;
   await getOriginalUrl(shortId);
 }
